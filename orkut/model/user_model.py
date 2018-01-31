ad4d9cacad70eeb22d1cdaa3984b4b6e61ff0b37
João Paulo Reis Alvarenga <joaopaulo@stilingue.com.br>
2018-01-31 17:12:31 -0200

    fix db.sql

--- orkut/model/user_model.py
+++ orkut/model/user_model.py
@@ -24,7 +24,7 @@ class UserModel(object):
                 cursor.execute('SELECT LAST_INSERT_ID()')
                 self.publishable = cursor.fetchone()[0]
 
-                cursor.execute('INSERT INTO usuarios (email, nome, dataNascimento, sexo, senha, codigoAtor, codigoPublicavel)'
+                cursor.execute('INSERT INTO usuarios (email, nome, data_nascimento, sexo, senha, codigo_ator, codigo_publicavel)'
                                'VALUES("{}", "{}", "{}", "{}", "{}", {}, {});'.format(self.email, self.name, self.birthdate, self.gender, self.password,
                                                                  self.actor, self.publishable))
                 cursor.execute('SELECT LAST_INSERT_ID();')
@@ -35,9 +35,9 @@ class UserModel(object):
     @staticmethod
     def find_friends(uid):
         with Config().get_db_connection().cursor() as cursor:
-            cursor.execute('SELECT codigo, email, nome, dataNascimento, sexo, senha, codigoAtor, codigoPublicavel '
+            cursor.execute('SELECT codigo, email, nome, data_nascimento, sexo, senha, codigo_ator, codigo_publicavel '
                            'FROM usuarios, sao_amigos '
-                           'WHERE sao_amigos.codigoUsuario1 = {} AND usuarios.codigo = sao_amigos.codigoUsuario2'
+                           'WHERE sao_amigos.codigo_usuario_1 = {} AND usuarios.codigo = sao_amigos.codigo_usuario_2'
                            .format(uid))
             for u in cursor.fetchall():
                 yield UserModel(id=u[0], email=u[1], name=u[2], birthdate=u[3], gender=u[4], password=u[5], actor=u[6],
@@ -46,7 +46,7 @@ class UserModel(object):
     @staticmethod
     def find_by_email_and_password(email, password):
         with Config().get_db_connection().cursor() as cursor:
-            cursor.execute('SELECT codigo, email, nome, dataNascimento, sexo, senha, codigoAtor, codigoPublicavel '
+            cursor.execute('SELECT codigo, email, nome, data_nascimento, sexo, senha, codigo_ator, codigo_publicavel '
                            'FROM usuarios '
                            'WHERE usuarios.email = \'{}\' AND usuarios.senha = \'{}\''.format(email, password))
             u = cursor.fetchone()
